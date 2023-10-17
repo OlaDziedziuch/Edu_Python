03288ee25864539859ffcb780187e1f263b1707d
olicze <aleksandradziedziuch@gmail.com>
2023-10-17 13:16:29 +0200

    New easy challenges are added

--- Edabit_easy/stand_in_line.py
+++ Edabit_easy/stand_in_line.py
@@ -5,7 +5,7 @@ def next_in_line(lst, n):
         return lst
     else: return "No list has been selected"
 
-print(next_in_line([5, 6, 7, 8, 9], 1))
-print(next_in_line([7, 6, 3, 23, 17], 10))
-print(next_in_line([7, 6, 3, 23, 17], 10))
-print(next_in_line([], 6))
+print(next_in_line([5, 6, 7, 8, 9], 1))             # ➞ [6, 7, 8, 9, 1]
+print(next_in_line([7, 6, 3, 23, 17], 10))          # ➞ [6, 3, 23, 17, 10]
+print(next_in_line([7, 6, 3, 23, 17], 10))          # ➞ [6, 3, 23, 17, 10]
+print(next_in_line([], 6))                          # ➞ No list has been selected
